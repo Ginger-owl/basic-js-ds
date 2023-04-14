2fcf90f12fb453ab8a384ea72213868c5d8e4184
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-04-15 01:24:40 +0400

    feat: implement binary search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,143 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.treeRoot = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.treeRoot
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    let newNode = new Node(data);
+    if(this.treeRoot === null){
+        this.treeRoot = newNode;
+        return this;
+    }
+    let current = this.treeRoot;
+    while(current){
+      if(data === current.data) return undefined;
+      if(data < current.data){
+        if(current.left === null){
+            current.left = newNode;
+            return this;
+        }
+        current = current.left;
+      } else {
+        if(current.right === null){
+            current.right = newNode;
+            return this;
+        } 
+        current = current.right;
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    if (!this.treeRoot) {
+      return null;
+    }
+    let current = this.treeRoot;
+    const hope = true;
+    while (hope) {
+      if (!current) {
+        return false;
+      }
+      if (data === current.data) {
+        return true;
+      } 
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    if(!this.treeRoot) {
+      return null
+    }
+    
+    let current = this.treeRoot
+    let found = null
+    while (current && !found) {
+      if (data < current.data) {
+        current = current.left
+        } else if (data > current.data) {
+          current = current.right
+        } else {
+          found = current
+        } 
+      }
+  
+      if (!found) {
+        return null;
+      }
+      return found
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    const recursiveDelete = (current, data) => {
+      if (current == null) {
+        return null
+      }
+      if (data < current.data) {
+        current.left = recursiveDelete(current.left, data)
+      }
+      else if (data > current.data) {
+        current.right = recursiveDelete(current.right, data)
+      }
+      else {
+        if (current.left === null) {
+          return current.right
+        } else if (current.right === null) {
+          return current.left
+        }
+
+        let inorderSuccessor = current.right
+        while (inorderSuccessor.left !== null) {
+          inorderSuccessor = inorderSuccessor.left
+        }
+        current.data = inorderSuccessor.data
+
+        current.right = recursiveDelete(current.right, inorderSuccessor.data)
+      }
+      return current
+    }
+    
+    this.treeRoot = recursiveDelete(this.treeRoot, data)
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.treeRoot) {
+      return null
+    }
+    
+    let current = this.treeRoot
+    while (current && current.left != null) {
+      current = current.left
+    }
+    return current.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.treeRoot) {
+      return null
+    }
+    
+    let current = this.treeRoot
+    while (current && current.right != null) {
+      current = current.right
+    }
+    return current.data
   }
 }
 
